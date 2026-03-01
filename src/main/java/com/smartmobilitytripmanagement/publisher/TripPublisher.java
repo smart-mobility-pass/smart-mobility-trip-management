9a6dc45493b67da5eea857903d4f4a97c1a431ea
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-01 10:51:38 +0000

    refactor TripPublisher to use constructor injection for RabbitTemplate

--- src/main/java/com/smartmobilitytripmanagement/publisher/TripPublisher.java
+++ src/main/java/com/smartmobilitytripmanagement/publisher/TripPublisher.java
@@ -2,17 +2,17 @@ package com.smartmobilitytripmanagement.publisher;
 
 import com.smartmobilitytripmanagement.config.RabbitMQConfig;
 import com.smartmobilitytripmanagement.dto.TripCompletedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class TripPublisher {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
     public void publishTripCompleted(TripCompletedEvent tripEvent) {
         log.info("Envoi d'un événement de trajet complété à RabbitMQ (ID: {})", tripEvent.getTripId());
