e67ec46887bdec76e1d9f7dbdd6759c4d03dfb31
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-01 10:52:14 +0000

    refactor UserProxy to update Feign client name and change userId type to String

--- src/main/java/com/smartmobilitytripmanagement/proxy/UserProxy.java
+++ src/main/java/com/smartmobilitytripmanagement/proxy/UserProxy.java
@@ -1,13 +1,12 @@
 package com.smartmobilitytripmanagement.proxy;
 
-
 import com.smartmobilitytripmanagement.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-mobility-service")
+@FeignClient(name = "user-mobility-pass-service")
 public interface UserProxy {
-    @GetMapping("/users/{id}")
-    UserDTO getUserById(@PathVariable("id") Long id);
+    @GetMapping("/api/users/{id}")
+    UserDTO getUserById(@PathVariable("id") String id);
 }
